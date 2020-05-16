fa321fcd2ea4bfbf5c95cfb77226a8586267d89a
yashu2001 <y_narang@yahoo.in>
2020-05-16 06:06:24 +0530

    Cleaning code and adding more comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+// Core react imports
 import React from 'react';
 import './App.css';
+// Css import
 import Layout from './components/Layout/Layout'
+// The main functional component that is imported into the index.js file
 function App() {
   return (
     <div className="App">
@@ -8,5 +11,5 @@ function App() {
     </div>
   );
 }
-
+// Exporting our component to facilitate import
 export default App;
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,17 +1,26 @@
+// Core react imports
 import React from 'react'
 import { Link } from 'react-router-dom'
+// Css import
 import classes from './SideBar.module.css'
+// The main functional component that imported inside of Layout
 export default function (props) {
+    // Checking if the status model is open or close
     if(props.status){
         var classlist=[classes.sidebar,classes.open]
     }
+    // JSX returned from the functional component
     return (
+        // Initial JSX setup
         <div className={classlist ?[classes.sidebar,classes.open].join(" ") : classes.sidebar}>
+            {/* JSX for button that handles sidebar close */}
             <div className={classes.closebtn} onClick={()=>props.setstatus(!props.status)}>x</div>
+            {/* JSX for sidebar header */}
             <div className={classes.sidebarheader}>
                 PMDB
             </div>
             <hr></hr>
+            {/* JSX for the sidebar list */}
             <ul className={classes.SideList}>
                 { props.loggedIn ? [
                 <li><Link to="/" className={classes.link} onClick={()=>props.setstatus(!props.status)}>Movies</Link></li>,
